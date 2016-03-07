20d4c0e7d05b20bbea9e4e7d7dd596824af21916
Andrew Berezovskyi <andrew@berezovskyi.me>
2016-03-07 14:08:05 +0100

    Fix typo

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ payload = {
         }
 headers = {'content-type': 'application/json'}
 
-endpoint_node = ENDPOINT_BASE + "prod/lambda-at-rest-node"
-endpoint_python = ENDPOINT_BASE + "prod/lambda-at-rest-python"
-endpoint_java = ENDPOINT_BASE + "prod/lambda-at-rest-java"
+endpoint_node = config.ENDPOINT_BASE + "prod/lambda-at-rest-node"
+endpoint_python = config.ENDPOINT_BASE + "prod/lambda-at-rest-python"
+endpoint_java = config.ENDPOINT_BASE + "prod/lambda-at-rest-java"
 
 def run_post(endpoint, payload):
     r = requests.post(endpoint, data=json.dumps(payload), headers=headers)
@@ -30,5 +30,5 @@ while True:
     run_post(endpoint_python, payload)
     time.sleep(random.uniform(0.1, 5))
     run_post(endpoint_java, payload)
-    time.sleep(random.uniform(2, 90))
+    time.sleep(random.uniform(90, 300))
     print("xxx")
